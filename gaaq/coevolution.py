d0925a195f22fc5e5904fe3d7cde592a9328cc71
Aleksandr Panov <panov.ai@ya.ru>
2016-07-27 17:24:43 +0300

    [~] Fix gaaq test

--- gaaq/coevolution.py
+++ gaaq/coevolution.py
@@ -1,9 +1,11 @@
-#coding=utf8
+# coding=utf8
 import numpy as np
-import population
-import ga_parameters as gap
-#from multiprocessing import Pool
-from concurrent.futures import ThreadPoolExecutor
+
+import gaaq.ga_parameters as gap
+from gaaq.population import cpopulation
+
+
+# from multiprocessing import Pool
 
 # Author: Alexander Shvets
 # Date: 29.06.2016
@@ -14,14 +16,17 @@ class ccoevolution:
         self.n = gap.pops_size
         self.pop = []
         self.buffpop = []
-        for i in range(self.cn-1):
-            p = population.cpopulation()
-            p.init(int(self.n / self.cn), processed_data, numval_per_gen, pos_index, neg_index, uncov_pos, 
-                   gap.ga_type[i], gap.sel_type[i], gap.tour_size[i], gap.elitism[i], gap.rec_type[i], gap.pop_part_rec[i], gap.nmut_bits[i], gap.pop_part_distr[i])
+        for i in range(self.cn - 1):
+            p = cpopulation()
+            p.init(int(self.n / self.cn), processed_data, numval_per_gen, pos_index, neg_index, uncov_pos,
+                   gap.ga_type[i], gap.sel_type[i], gap.tour_size[i], gap.elitism[i], gap.rec_type[i],
+                   gap.pop_part_rec[i], gap.nmut_bits[i], gap.pop_part_distr[i])
             self.pop.append(p)
-        p = population.cpopulation()
-        p.init(int(self.n / self.cn)+int(self.n % self.cn), processed_data, numval_per_gen, pos_index, neg_index, uncov_pos, 
-                   gap.ga_type[-1], gap.sel_type[-1], gap.tour_size[-1], gap.elitism[-1], gap.rec_type[-1], gap.pop_part_rec[-1], gap.nmut_bits[-1], gap.pop_part_distr[-1])
+        p = cpopulation()
+        p.init(int(self.n / self.cn) + int(self.n % self.cn), processed_data, numval_per_gen, pos_index, neg_index,
+               uncov_pos,
+               gap.ga_type[-1], gap.sel_type[-1], gap.tour_size[-1], gap.elitism[-1], gap.rec_type[-1],
+               gap.pop_part_rec[-1], gap.nmut_bits[-1], gap.pop_part_distr[-1])
         self.pop.append(p)
         self.socialcard = int(0.05 * (self.n))
         self.socialfine = int(0.04 * (self.n / self.cn))
@@ -30,26 +35,25 @@ class ccoevolution:
         self.coevbestgenotype = []
         self.coevbestgenotypefit = - np.inf
         self.coev_popstop = False
-        self.generalgroup = population.cpopulation()
-        self.losematrix = np.zeros((self.cn, self.cn), dtype = int)
-        self.penlty = np.zeros((self.cn), dtype = int)
-           
+        self.generalgroup = cpopulation()
+        self.losematrix = np.zeros((self.cn, self.cn), dtype=int)
+        self.penlty = np.zeros((self.cn), dtype=int)
 
     def adaptation(self):
-        #def adaptation_period(self, p):
+        # def adaptation_period(self, p):
         #    p.adaptfits = []
         #    for g in range(self.nadapt):
         #        p.one_ga_step()        
-        #pl = Pool(self.cn)
-        #pl.map(adaptation_period, self.pop)
-        
-        #slower on 35-40% then running one algorithm
-        
+        # pl = Pool(self.cn)
+        # pl.map(adaptation_period, self.pop)
+
+        # slower on 35-40% then running one algorithm
+
         for pind, p in enumerate(self.pop):
             p.adaptfits = []
             for g in range(self.nadapt):
                 p.one_ga_step()
-            #print(pind, p.bestfit)
+                # print(pind, p.bestfit)
         '''
         def worker(pind):
             self.pop[pind].adaptfits = []
@@ -73,25 +77,26 @@ class ccoevolution:
                 elif (p.adaptfits[g] == bestalgfit):
                     bestalgindexes.append(ind)
             for balg in bestalgindexes:
-                self.pop[balg].rating += (g + 1)*1.0 / (self.nadapt - g)
+                self.pop[balg].rating += (g + 1) * 1.0 / (self.nadapt - g)
         return
-    
+
     def moveToGeneralGroup(self):
         data_indexes = []
         for i in range(self.cn):
             for j in range(self.pop[i].psize):
-                data_indexes.append((i, j))                 
+                data_indexes.append((i, j))
         sorted_indexes = range(self.n)
-        sorted_indexes = sorted(sorted_indexes, key = lambda i: -self.pop[data_indexes[i][0]].fits[data_indexes[i][1]]) # descending order     
-        
+        sorted_indexes = sorted(sorted_indexes, key=lambda i: -self.pop[data_indexes[i][0]].fits[
+            data_indexes[i][1]])  # descending order
+
         generalgroup = np.copy(self.pop[data_indexes[sorted_indexes[0]][0]].data[data_indexes[sorted_indexes[0]][1]])
         generalfits = np.copy(self.pop[data_indexes[sorted_indexes[0]][0]].fits[data_indexes[sorted_indexes[0]][1]])
         for ind in sorted_indexes[1:]:
             generalgroup = np.vstack((generalgroup, np.copy(self.pop[data_indexes[ind][0]].data[data_indexes[ind][1]])))
             generalfits = np.hstack((generalfits, np.copy(self.pop[data_indexes[ind][0]].fits[data_indexes[ind][1]])))
-        
+
         return generalgroup, generalfits
-    
+
     def moveOutOfGeneralGroup(self, generalgroup, generalfits):
         for p in self.pop:
             p.data = np.copy(generalgroup[:p.psize])
@@ -103,22 +108,26 @@ class ccoevolution:
                 self.coevbestgenotype = np.copy(p.bestgenotype)
                 self.coevbestgenotypefit = p.bestgenotypefit
         return
-    
+
     def moveOutOfGeneralGroup2(self, generalgroup, generalfits):
         for p in self.pop:
             sorted_indexes = range(p.prevpsize)
-            sorted_indexes = sorted(sorted_indexes, key = lambda i: -p.fits[i]) # descending order
+            sorted_indexes = sorted(sorted_indexes, key=lambda i: -p.fits[i])  # descending order
             if (p.psize < p.prevpsize):
-                p.data[sorted_indexes[(p.psize-int(self.socialcard/2.0)):p.psize]] = np.copy(generalgroup[:int(self.socialcard/2.0)])
-                p.fits[sorted_indexes[(p.psize-int(self.socialcard/2.0)):p.psize]] = np.copy(generalfits[:int(self.socialcard/2.0)])
+                p.data[sorted_indexes[(p.psize - int(self.socialcard / 2.0)):p.psize]] = np.copy(
+                    generalgroup[:int(self.socialcard / 2.0)])
+                p.fits[sorted_indexes[(p.psize - int(self.socialcard / 2.0)):p.psize]] = np.copy(
+                    generalfits[:int(self.socialcard / 2.0)])
                 p.data = np.delete(p.data, sorted_indexes[p.psize:], axis=0)
                 p.fits = np.delete(p.fits, sorted_indexes[p.psize:])
             else:
                 while (len(p.data) < p.psize):
-                    p.data = np.vstack((p.data, np.copy(p.data[sorted_indexes[:(p.psize-p.prevpsize)]])))
-                    p.fits = np.hstack((p.fits, np.copy(p.fits[sorted_indexes[:(p.psize-p.prevpsize)]])))
-                p.data[sorted_indexes[(p.prevpsize-int(self.socialcard/2.0)):p.prevpsize]] = np.copy(generalgroup[:int(self.socialcard/2.0)])
-                p.fits[sorted_indexes[(p.prevpsize-int(self.socialcard/2.0)):p.prevpsize]] = np.copy(generalfits[:int(self.socialcard/2.0)])
+                    p.data = np.vstack((p.data, np.copy(p.data[sorted_indexes[:(p.psize - p.prevpsize)]])))
+                    p.fits = np.hstack((p.fits, np.copy(p.fits[sorted_indexes[:(p.psize - p.prevpsize)]])))
+                p.data[sorted_indexes[(p.prevpsize - int(self.socialcard / 2.0)):p.prevpsize]] = np.copy(
+                    generalgroup[:int(self.socialcard / 2.0)])
+                p.fits[sorted_indexes[(p.prevpsize - int(self.socialcard / 2.0)):p.prevpsize]] = np.copy(
+                    generalfits[:int(self.socialcard / 2.0)])
             if p.bestgenotypefit < generalfits[0]:
                 p.bestgenotype = np.copy(generalgroup[0])
                 p.bestgenotypefit = generalfits[0]
@@ -126,15 +135,15 @@ class ccoevolution:
                 self.coevbestgenotype = np.copy(p.bestgenotype)
                 self.coevbestgenotypefit = p.bestgenotypefit
         return
-    
+
     def changeResourses(self):
         generalgroup, generalfits = self.moveToGeneralGroup()
 
         for p in self.pop:
-            p.prevpsize = p.psize;
+            p.prevpsize = p.psize
 
-        self.losematrix = np.zeros((self.cn, self.cn), dtype = int)
-        self.penlty = np.zeros((self.cn), dtype = int)
+        self.losematrix = np.zeros((self.cn, self.cn), dtype=int)
+        self.penlty = np.zeros((self.cn), dtype=int)
         for i in range(self.cn):
             numwins = 0
             for j in range(self.cn):
@@ -144,31 +153,33 @@ class ccoevolution:
 
             if (self.pop[i].psize > self.socialcard):
                 if ((self.pop[i].psize - (numwins * self.socialfine)) <= self.socialcard):
-                    self.penlty[i] = int((self.pop[i].psize - self.socialcard)*1.0 / numwins)
+                    self.penlty[i] = int((self.pop[i].psize - self.socialcard) * 1.0 / numwins)
                 else:
                     self.penlty[i] = self.socialfine
 
         for i in range(self.cn):
             for j in range(self.cn):
                 if (self.losematrix[i][j] == 1):
-                    self.pop[i].psize -= self.penlty[i] # probably penalties should be less
+                    self.pop[i].psize -= self.penlty[i]  # probably penalties should be less
                     self.pop[j].psize += self.penlty[i]
 
-        #self.MoveOutOfGeneralGroup(generalgroup, generalfits)
+        # self.MoveOutOfGeneralGroup(generalgroup, generalfits)
         self.moveOutOfGeneralGroup2(generalgroup, generalfits)
         return
-                
-    # END OF CLASS CCOEVOLUTION
-    
+
+        # END OF CLASS CCOEVOLUTION
+
+
 def run_coev_gaaq(processed_data, numval_per_gen, pos_index, neg_index, uncov_pos):
     coev_pop = ccoevolution(processed_data, numval_per_gen, pos_index, neg_index, uncov_pos)
     for i in range(gap.ngen):
         coev_pop.adaptation()
         coev_pop.changeResourses()
         print(i, coev_pop.coevbestgenotypefit)
-        
-    if(len(coev_pop.coevbestgenotype != 0)):
-        neg_cov, num_new_covered, num_all_not_miss, num_miss, rule_coverage, rule_exactcoverage, genes_has_ones = coev_pop.pop[0].check_coverage(coev_pop.coevbestgenotype)
+
+    if len(coev_pop.coevbestgenotype):
+        neg_cov, num_new_covered, num_all_not_miss, num_miss, rule_coverage, rule_exactcoverage, genes_has_ones = \
+            coev_pop.pop[0].check_coverage(coev_pop.coevbestgenotype)
     else:
         neg_cov = num_new_covered = num_all_not_miss = num_miss = 0
         rule_coverage = []
--- gaaq/main.py
+++ gaaq/main.py
@@ -1,59 +1,66 @@
-#coding=utf8
-import arff, numpy as np
-import numpy.random as rand
-from collections import Counter, defaultdict
-import population
-import coevolution
-import cProfile, pstats
+# coding=utf8
+import cProfile
 import io
+import pstats
+from collections import Counter, defaultdict
+
+import arff
+import numpy as np
+import numpy.random as rand
+
+from gaaq.coevolution import run_coev_gaaq
+
 
 # Author: Alexander Shvets
 # Date: 29.06.2016
 
 def load_data(arff_file):
     return arff.load(open(arff_file, 'r'))
-  
+
+
 def preprocess_data(dataset, NumRealIntervals):
     data = np.array(dataset['data'], dtype=np.float)
-    class_labels = np.array(data[:,-1], dtype = int)
-    
-    numAttr = len(dataset['attributes'])-1 #minus 1 - for class
-    
+    class_labels = np.array(data[:, -1], dtype=int)
+
+    numAttr = len(dataset['attributes']) - 1  # minus 1 - for class
+
     real_values = np.array(list(i for i, attr in enumerate(dataset['attributes']) if attr[1] == 'REAL'))
     min_real = np.nanmin(data[:, real_values], axis=0)
     max_real = np.nanmax(data[:, real_values], axis=0)
-    inter_size = (max_real - min_real)*1.0 / NumRealIntervals
+    inter_size = (max_real - min_real) * 1.0 / NumRealIntervals
 
-    real_mins = np.zeros(numAttr+1)
-    real_inters = np.zeros(numAttr+1)
+    real_mins = np.zeros(numAttr + 1)
+    real_inters = np.zeros(numAttr + 1)
     real_mins[real_values] = min_real
     real_inters[real_values] = inter_size
 
-    numval_per_gen = np.array([len(attr[1]) if attr[1] != 'REAL' else NumRealIntervals for attr in dataset['attributes'][:-1]]) # sizes of features
+    numval_per_gen = np.array([len(attr[1]) if attr[1] != 'REAL' else NumRealIntervals for attr in
+                               dataset['attributes'][:-1]])  # sizes of features
 
     processed_data = []
     for d in data:
         d0 = d[:-1]
         for rv in real_values:
-            if(not np.isnan(d0[rv])):
+            if (not np.isnan(d0[rv])):
                 for numi in range(NumRealIntervals):
-                    if(d0[rv] <= real_mins[rv]+(numi+1)*1.0*real_inters[rv]):
+                    if (d0[rv] <= real_mins[rv] + (numi + 1) * 1.0 * real_inters[rv]):
                         d0[rv] = numi
                         break
         processed_data.append(d0)
-    processed_data = np.array(processed_data)  
+    processed_data = np.array(processed_data)
     print("Data have been loaded and preprocessed.", Counter(list(d for d in class_labels)))
     return processed_data, class_labels, numval_per_gen
 
+
 def get_examples_indexes(processed_data, class_labels, classLabel):
     numAttr = len(processed_data[0])
-    #pos_index = np.array([int(d[-1])==int(classLabel) for d in data])
-    #neg_index = np.invert(pos_index)
+    # pos_index = np.array([int(d[-1])==int(classLabel) for d in data])
+    # neg_index = np.invert(pos_index)
 
-    pos_index = np.array([i for i,d in enumerate(class_labels) if d==classLabel])
-    neg_index = np.array([i for i,d in enumerate(class_labels) if d!=classLabel])
+    pos_index = np.array([i for i, d in enumerate(class_labels) if d == classLabel])
+    neg_index = np.array([i for i, d in enumerate(class_labels) if d != classLabel])
 
-    #remove conflict fobj
+    # remove conflict fobj
     to_del = []
     for i, aNeg in enumerate(processed_data[neg_index]):
         found = False
@@ -66,21 +73,24 @@ def get_examples_indexes(processed_data, class_labels, classLabel):
             if (found):
                 to_del.append(i)
                 break
-    print(("Found and deleted %d conflict negative objects (" % len(to_del)) + (" ".join(str(k) for k in neg_index[to_del])) + ")")
+    print(("Found and deleted %d conflict negative objects (" % len(to_del)) + (
+        " ".join(str(k) for k in neg_index[to_del])) + ")")
     neg_index = np.delete(neg_index, to_del)
 
-    uncov_pos = defaultdict(lambda:1) # left positive examples that are still not covered
+    uncov_pos = defaultdict(lambda: 1)  # left positive examples that are still not covered
     for pind in pos_index:
         uncov_pos[pind]
-    
+
     return pos_index, neg_index, uncov_pos
 
-#temporary
+
+# temporary
 class AQRule():
     def __init__(self):
         self.id = 0
         self.forceCoverage = 0
         self.features = []
+
 
 def evaluate_AQ_rules(processed_data, class_labels, numval_per_gen):
     numAttr = len(processed_data[0])
@@ -104,56 +114,57 @@ def evaluate_AQ_rules(processed_data, class_labels, numval_per_gen):
         rule_genotypes = []
         without_result = 0
         breakWithoutResult = 5
-        
+
         while (len(uncov_pos) != 0):
-            #GA           
-            #neg_cov, num_new_covered, num_all_not_miss, num_miss, rule_coverage, rule_exactcoverage, genes_has_ones, best_bits, best_fit = population.run_gaaq(processed_data, numval_per_gen, pos_index, neg_index, uncov_pos)
-            #Coevolutionary GA
-            neg_cov, num_new_covered, num_all_not_miss, num_miss, rule_coverage, rule_exactcoverage, genes_has_ones, best_bits, best_fit = coevolution.run_coev_gaaq(processed_data, numval_per_gen, pos_index, neg_index, uncov_pos)
-            
-            if(num_new_covered == 0):
+            # GA
+            # neg_cov, num_new_covered, num_all_not_miss, num_miss, rule_coverage, rule_exactcoverage, genes_has_ones, best_bits, best_fit = population.run_gaaq(processed_data, numval_per_gen, pos_index, neg_index, uncov_pos)
+            # Coevolutionary GA
+            neg_cov, num_new_covered, num_all_not_miss, num_miss, rule_coverage, rule_exactcoverage, genes_has_ones, best_bits, best_fit = run_coev_gaaq(
+                processed_data, numval_per_gen, pos_index, neg_index, uncov_pos)
+
+            if (num_new_covered == 0):
                 print("No new positive example has been covered")
-            if(neg_cov):
+            if (neg_cov):
                 print("Decision is not appropriate. Negative examples have been covered")
-            if((num_new_covered == 0) or (neg_cov)):
+            if ((num_new_covered == 0) or (neg_cov)):
                 without_result += 1
                 if (without_result == breakWithoutResult):
                     break
                 else:
                     continue
             without_result = 0
-            
-            #if the feature covers any meaningful value in positive example (covered by rule)
-                #and there exist different value of this feature in other positive example (not necessary covered by rule), then this feature could be essential
-            #if all positive examples have the same or missing value of this feature or positive examples covered by rule have only missing values for this feature,
-                    #then the feature is not essential in this rule in case that there is no different value for this feature in negative examples
+
+            # if the feature covers any meaningful value in positive example (covered by rule)
+            # and there exist different value of this feature in other positive example (not necessary covered by rule), then this feature could be essential
+            # if all positive examples have the same or missing value of this feature or positive examples covered by rule have only missing values for this feature,
+            # then the feature is not essential in this rule in case that there is no different value for this feature in negative examples
             ess = []
             for j in genes_has_ones:
                 ess_bool = False
                 for aPos in processed_data[rule_coverage]:
-                    if (aPos[j]==aPos[j]):
+                    if (aPos[j] == aPos[j]):
                         ess_bool = True
                         break
-                if(ess_bool):
+                if (ess_bool):
                     ess_bool = False
                     for aPos in processed_data[pos_index]:
-                        if ((aPos[j]==aPos[j]) and (best_bits[cum_gens[j]+int(aPos[j])] == 0)):
+                        if ((aPos[j] == aPos[j]) and (best_bits[cum_gens[j] + int(aPos[j])] == 0)):
                             ess_bool = True
                             break
-                if(not ess_bool):#not else!
+                if (not ess_bool):  # not else!
                     for aNeg in processed_data[neg_index]:
-                        if ((aNeg[j]==aNeg[j]) and (best_bits[cum_gens[j]+int(aNeg[j])] == 0)):
+                        if ((aNeg[j] == aNeg[j]) and (best_bits[cum_gens[j] + int(aNeg[j])] == 0)):
                             ess_bool = True
                             break
-                if(ess_bool):
+                if (ess_bool):
                     ess.append(j)
             essential.append(ess)
 
             newobj_cov = []
             for rc in rule_coverage:
-                if(rc in uncov_pos):
+                if (rc in uncov_pos):
                     newobj_cov.append(rc)
-                    del uncov_pos[rc]                        
+                    del uncov_pos[rc]
 
             num_objects.append(num_all_not_miss + num_miss)
             num_miss_objects.append(num_miss)
@@ -166,52 +177,56 @@ def evaluate_AQ_rules(processed_data, class_labels, numval_per_gen):
             print("all_objects = %d" % (num_all_not_miss + num_miss))
             print("objects_with_missing_value = %d" % num_miss)
             print("covered_objects = %d" % covered_objects)
-            print("left_objects = %d" % (len(pos_index)-covered_objects))
+            print("left_objects = %d" % (len(pos_index) - covered_objects))
 
             rule_genotypes.append((best_bits, best_fit))
-            #end of while-cycle
-            
+            # end of while-cycle
+
         result = ""
-        result += ("NUM_UNCOVERED_LEFT_OBJECTS = %d (" % len(uncov_pos)) +  " ".join(str(k) for k in uncov_pos) + ")\n"
+        result += ("NUM_UNCOVERED_LEFT_OBJECTS = %d (" % len(uncov_pos)) + " ".join(str(k) for k in uncov_pos) + ")\n"
         for i in range(len(rule_genotypes)):
-            result += "RULE_%d:\n" % (i+1)
-            result += ("\tNUM_NEW_OBJECTS: %d (" % (num_new_objects[i])) +  " ".join(str(k) for k in newobj_cov_by_rule[i]) + ")\n"
-            result += ("\tNUM_OBJECTS: %d (" % (num_objects[i])) +  " ".join(str(k) for k in obj_cov_by_rule[i]) + ")\n"
-            result += ("\tNUM_EXACT_OBJECTS: %d (" % (num_objects[i] - num_miss_objects[i])) + " ".join(str(k) for k in exactobj_cov_by_rule[i]) + ")\n"
+            result += "RULE_%d:\n" % (i + 1)
+            result += ("\tNUM_NEW_OBJECTS: %d (" % (num_new_objects[i])) + " ".join(
+                str(k) for k in newobj_cov_by_rule[i]) + ")\n"
+            result += ("\tNUM_OBJECTS: %d (" % (num_objects[i])) + " ".join(str(k) for k in obj_cov_by_rule[i]) + ")\n"
+            result += ("\tNUM_EXACT_OBJECTS: %d (" % (num_objects[i] - num_miss_objects[i])) + " ".join(
+                str(k) for k in exactobj_cov_by_rule[i]) + ")\n"
             result += "\tNUM_MISS_OBJECTS: %d\n" % (num_miss_objects[i])
-            rule = AQRule() # AQRule rule = new AQRule();
-            rule.id = i # rule.setId(bp);
-            rule.forceCoverage = num_objects[i] # rule.setForceCoverage(num_objects.get(bp));
+            rule = AQRule()  # AQRule rule = new AQRule();
+            rule.id = i  # rule.setId(bp);
+            rule.forceCoverage = num_objects[i]  # rule.setForceCoverage(num_objects.get(bp));
             for j in essential[i]:
-                genbits = rule_genotypes[i][0][cum_gens[j]:cum_gens[j+1]]
-                ones_pos = [k+1 for k in range(len(genbits)) if (genbits[k]==1)] # k+1 because positions in AQRules are numerated from 1
-                rule.features.append((j, ones_pos)) #rule.getTokens().put(featureMap.get(i), ones_pos_limited); # no need to evaluate ones_pos_limited now
-                result += ("\tattr_%d = " % (j)) + " ".join(str(k) for k in ones_pos) + "\n" # attributes for some reason are numerated from 0
+                genbits = rule_genotypes[i][0][cum_gens[j]:cum_gens[j + 1]]
+                ones_pos = [k + 1 for k in range(len(genbits)) if
+                            (genbits[k] == 1)]  # k+1 because positions in AQRules are numerated from 1
+                rule.features.append((j,
+                                      ones_pos))  # rule.getTokens().put(featureMap.get(i), ones_pos_limited); # no need to evaluate ones_pos_limited now
+                result += ("\tattr_%d = " % (j)) + " ".join(
+                    str(k) for k in ones_pos) + "\n"  # attributes for some reason are numerated from 0
             classRules.append(rule)
             result += "\n"
-        
-        #Collections.sort(classRules);
-        #classMapDescriptions.put(className, AQClassDescription.createFromRules(classRules, maximumDescriptionSize, className));
+
+        # Collections.sort(classRules);
+        # classMapDescriptions.put(className, AQClassDescription.createFromRules(classRules, maximumDescriptionSize, className));
         print(result)
-        #end of for-cycle   
+        # end of for-cycle
     return
-            
-            
-if __name__ == '__main__':            
-    #start profiling
+
+
+if __name__ == '__main__':
+    # start profiling
     pr = cProfile.Profile()
     pr.enable()
-    
-    NumRealIntervals = 3 # number of intervals into which we will map real values
+
+    NumRealIntervals = 3  # number of intervals into which we will map real values
     dataset = load_data("class_17_without_missing_16and18.arff")
     processed_data, class_labels, numval_per_gen = preprocess_data(dataset, NumRealIntervals)
     evaluate_AQ_rules(processed_data, class_labels, numval_per_gen)
 
-    #stop profiling
+    # stop profiling
     pr.disable()
     s = io.StringIO()
-    sortby = 'time'#'cumulative'
+    sortby = 'time'  # 'cumulative'
     ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
     ps.print_stats(10)
     print(s.getvalue())
-    
