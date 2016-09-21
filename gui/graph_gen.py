063e133ef27542ce0ec30799ce1baa0c43dc79d2
andy <hunter_hunternabab@mail.ru>
2016-09-22 01:26:01 +0300

    posledovatelnye rebra vershini

--- gui/graph_gen.py
+++ gui/graph_gen.py
@@ -39,7 +39,7 @@ def write_html():
       <td style="min-width:200px; width:15%; background-color:#F7EC8D";>'''
 
     str2 = '''<div>
-      <p id="result"><b>Для выполнения целей: <br></b>'''
+      <p id="result"><b>Для выполнения целей:  УБРАТЬ !!!! <br></b>'''
     for i in range(len(propetis)):
         str2 += propetis[i] + '; '
 
@@ -114,11 +114,97 @@ def write_html():
     '''
     f.write(str1 + str2 + str3 + str4 + str5 + str6 + str7 + str8 + str9 + str10 + str11)
 
+def old_draw():
+    count_line_prop = 3  # point in line prop
+    count_line_reas = 2  # point in line reas
+    smesh_row = 0  # left margin of next row
+    scale = 1  # space near node
+
+    count_node = 0
+    x_t = 1 * scale
+    y_t = 1 * scale
+
+    mas_edge = []
+    mas_reas = []
+    mas_prop = []
+    mas_link = []
+
+    select_color = 0
+    save_reas = ''''''
+    last_id = 0
+    name = 0
+
+    for i in range(leng):
+        for j in range(lengmass):
+            if t1[i].value[j] == True:
+                if mas_reas.count(j) > 0:
+                    mas_edge.append(mas_edge[j])
+                else:
+                    mas_edge.append(last_id)
+                lab = names_reas[j + 1]  # Reasons name   from data_frame
+                if mas_reas.count(j) < 1:
+                    if (count_node % count_line_reas == 0) & (j > 0):
+                        y_t += 2 * scale
+                        x_t = smesh_row + 1 * scale
+                    G.add_node(last_id, x=x_t, y=y_t, size=size_node, label=lab, color='darkred')  # add node reas
+                    print(last_id, "\t", lab)
+                    last_id += 1
+                    x_t += 1 * scale
+                    count_node += 1
+                mas_reas.append(j)
+            else:
+                mas_edge.append(None)
+        save_reas += '<br>'
+        mas_reas.append(None)
+
+    num = mas_edge.count(None)
+    for i in range(num):
+        mas_edge.remove(None)
+
+    count_node = 0
+    x_t = 1 * scale
+    y_t = 1 * scale
+
+    for i in range(leng):
+        if (count_node % count_line_prop == 0) & (i > 0):
+            y_t += 1 * scale
+            x_t = smesh_row + 1 * scale
+        lab = 'Свой-во ' + str(i)  # Prooertise name
+        # G.add_node(last_id, x=x_t, y=y_t, size=size_node, label=lab, color=colors[select_color])  # add node reas
+        mas_prop.append(last_id)
+        x_t += 1 * scale
+        last_id += 1
+        select_color += 1
+        count_node += 1
+
+    print('reas:   ', mas_reas)
+    print('edge:   ', mas_edge)
+    print('prop:   ', mas_prop)
+
+    step_prop = 0
+    num = mas_edge[0]
+    select_color = 0
+
+    for i in range(len(mas_edge)):
+        if mas_edge[i] < num:
+            step_prop += 1
+            select_color += 1
+            # G.add_edge(mas_prop[step_prop], mas_edge[i], id=i, color=colors[select_color])
+            print('m: ', mas_edge[i])
+        else:
+            # G.add_edge(mas_prop[step_prop], mas_edge[i], id=i, color=colors[select_color])
+            print('m: ', mas_edge[i])
+
+        num = mas_edge[i]
+
+
 # t1 = jm.test1()
 t1 = jm.test2()  # test square
 
 data = pd.read_csv('ex1.csv', encoding='cp1251', sep=';', index_col=False, na_values='?')
 names_reas = list(data.columns.values)
+
+# print(data)
 
 reasons_square = []
 propetis = []
@@ -126,32 +212,30 @@ propetis = []
 br = '<br>'
 
 G = nx.Graph()
-colors = ['gray', 'green', 'pink', 'yellow', 'blue', 'orange']
+colors = ['green', 'darkred', 'brown', 'yellow', 'blue', 'orange']
 size_node = 8
 
 leng=len(t1) # count of pairs in hypotheses
 lengmass=len(t1[0].value) # count of reasons
 
-
-count_line_prop = 3   # point in line prop
-count_line_reas = 3   # point in line reas
-smesh_row=0     # left margin of next row
-scale=1         # space near node
+count_line_prop = 3  # point in line prop
+count_line_reas = 2  # point in line reas
+smesh_row = 0  # left margin of next row
+scale = 1  # space near node
 
 count_node = 0
-x_t=1*scale
-y_t=1*scale
+x_t = 1 * scale
+y_t = 1 * scale
 
-mas_edge=[]
-mas_reas=[]
-mas_prop=[]
-mas_link=[]
-
-
+save_reas = ''' save_REAS    text'''
+mas_edge = []
+mas_reas = []
+mas_prop = []
 select_color = 0
-save_reas =''''''
-last_id=0
-name=0
+last_id = 0
+
+
+
 
 for i in range(leng):
     for j in range(lengmass):
@@ -160,61 +244,64 @@ for i in range(leng):
                 mas_edge.append(mas_edge[j])
             else:
                 mas_edge.append(last_id)
-            lab = 'Reas ' + names_reas[j+1]  # Reasons name   from data_frame
-            if mas_reas.count(j)<1:
+            lab = names_reas[j + 1]  # Reasons name   from data_frame
+            if mas_reas.count(j) < 1:
                 if (count_node % count_line_reas == 0) & (j > 0):
                     y_t += 2 * scale
                     x_t = smesh_row + 1 * scale
                 G.add_node(last_id, x=x_t, y=y_t, size=size_node, label=lab, color='darkred')  # add node reas
-                print(last_id, "\t", lab)
+                # print(last_id, "\t", lab)
                 last_id += 1
                 x_t += 1 * scale
                 count_node += 1
             mas_reas.append(j)
         else:
             mas_edge.append(None)
-    save_reas+='<br>'
     mas_reas.append(None)
+    # print('endpart\n')
 
-num=mas_edge.count(None)
+# print(mas_edge)
+# print(mas_reas)
+
+
+
+num = mas_edge.count(None)
 for i in range(num):
     mas_edge.remove(None)
 
-count_node=0
-x_t=1*scale
-y_t=1*scale
 
-for i in range(leng):
-    if (count_node % count_line_prop == 0) & (i > 0):
-        y_t += 1 * scale
-        x_t = smesh_row + 1 * scale
-    lab = 'Свой-во ' + str(i)  # Prooertise name
-    G.add_node(last_id, x=x_t, y=y_t, size=size_node, label=lab, color=colors[select_color])  # add node reas
-    mas_prop.append(last_id)
-    x_t += 1 * scale
-    last_id += 1
-    select_color+=1
-    count_node += 1
+for i in range(len(mas_reas)):
+    if mas_reas[i]==None:
+        mas_edge.insert(i, None)
 
-print('reas:   ', mas_reas)
-print('edge:   ', mas_edge)
-print('prop:   ', mas_prop)
+print(mas_edge)
 
-step_prop=0
-num=mas_edge[0]
-select_color=0
 
-for i in range(len(mas_edge)):
-    if mas_edge[i]<num:
-        step_prop+=1
+
+source=mas_edge[0]
+target=mas_edge[1]
+for i in range(len(mas_edge)-2):
+
+    if source == None or target == None:
+        source=mas_edge[i+1]
+        target=mas_edge[i+2]
         select_color+=1
-        G.add_edge(mas_prop[step_prop], mas_edge[i], id=i, color=colors[select_color])
-        print('m: ',mas_edge[i])
     else:
-        G.add_edge(mas_prop[step_prop], mas_edge[i], id=i, color=colors[select_color])
-        print('m: ', mas_edge[i])
+        G.add_edge(source, target, id=i, color=colors[select_color])
+        print('sour: ', source)
+        print('targ: ', target)
+        print('\n')
 
-    num=mas_edge[i]
+        source = mas_edge[i+1]
+        target = mas_edge[i+2]
+        # G.add_edge(mas_prop[step_prop], mas_edge[i], id=i, color=colors[select_color])
+
+
+    # else:
+        # G.add_edge(mas_prop[step_prop], mas_edge[i], id=i, color=colors[select_color])
+        # print('m: ', mas_edge[i])
+
+
 
 
 d = json_graph.node_link_data(G)
@@ -228,7 +315,7 @@ print(s)
 
 write_html()
 
-pos = nx.circular_layout(G)
-nx.draw_networkx(G, pos=pos, with_labels=True)
-plt.show()
+# pos = nx.circular_layout(G)
+# nx.draw_networkx(G, pos=pos, with_labels=True)
+# plt.show()
 
