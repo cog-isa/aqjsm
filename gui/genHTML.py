fffd7215e86a9bef67eb8699d4044e22bd1f1d47
fiveh <hunter_hunternabab@mail.ru>
2016-10-02 21:37:23 +0300

    last try for use Flask to gen Html

--- gui/genHTML.py
+++ gui/genHTML.py
@@ -1,97 +1,61 @@
-def write_html(s):
+from gui import graph_gen as gg
 
-    reasons_square = []
-    propetis = []
+def writeHtml():
     br = '<br>'
-
-
-# addd testtt
-
-    save_reas = ''' save_REAS    text'''
-
     f = open('test.html', 'w', encoding='utf-8')
     str1 = '''
-
     <html>
     <head>
     <meta charset="Utf-8">
       <style type="text/css">
       #container {
-        max-width: 800px;
-        height: 800px;
-        margin-left: 15%;
+        min-width: 600px;
+        min-height: 600px;
+        margin-left: 5%;
         background-color:#DFEAFB;
-
       }
       #bigtable {
         background-color: gray;
-
       }
       #lefttext {
         background-color:#FFC3C3;
-
       }
 
     </style>
     </head>
     <body>
-      <table id="bigtable" border="2">
+      <table id="bigtable" border="2" align="center">
       <tr>
-      <td style="min-width:200px; width:15%; background-color:#F7EC8D";>'''
-
-    str2 = '''<div>
-      <p id="result"><b>Для выполнения целей:  УБРАТЬ !!!! <br></b>'''
-    # for i in data['P']:
-    #     if data[i] != 0 and data[i] !=1:
-    #         str2 += ';    EMPTY PLACE'
-
-
-    str3 = '''<br><b>Необходимо обладание причин: <br></b>''' + save_reas + '''</p></div>'''
-
-    str4 = '''
+        <td style="min-width:200px; width:15%; background-color:#F7EC8D";><div></div>
+      <p id="result"><b>*** Empty place ***<br></b>
       </td>
-      <td style="min-width:810px; width:60%; background-color:#A7FBA9;">
+      <td style="width:80%; background-color:#A7FBA9;">
           <div id="container"></div>
       </td>
 
       <td style="min-width:200px; width:25%";>
       <div style="background-color:#8B98F9">
-        <b>Цели:</b><br>
-      '''
+        <b>*** Empty place ***</b><br>
 
-    # for properties
-
-    str5 = ''''''
-    for i in range(len(propetis)):
-        propetis[i] += br
-        str5 += str(i + 1) + propetis[i]
-
-    str6 = '''
       </div>
         <div id="lefttext">
-        <b>Список причин:</b><br>
-        '''
-
-    # THIS  PLACE
-    tempstr = ''''''
-    for i in range(len(reasons_square)):
-        reasons_square[i] += br
-        tempstr += str(i) + reasons_square[i]
-
-    str7 = tempstr
-    str8 = ''' </div>
+        <b>*** Empty place ***</b><br>
+         </div>
       </td>
+        </tr>
+        <tr style="height: 50%">
+          <td colspan="3" align="center" style="background-color: #FFECEC">LOOOG!!</td>
         </tr>
       </table>
       <script src="sigma.js"></script>
       <script src="sigma.parsers.json.min.js"></script>
       <div id = "myid2" name="text" style="height:1px; width: 100%; visibility:hidden;">
       '''
-    str9 = s
-    str10 = '''
+    str2 = gg.s
+    str3 = '''
       </div>'''
 
-    str11 = '''
+    str4 = '''
       <script>
        var x = document.getElementById("myid2").innerHTML;
       // document.getElementById("result").innerHTML = x;
@@ -114,5 +78,10 @@ def write_html(s):
       <p align="center">end &copy;</p>
     </body>
     </html>
+
+
     '''
-    f.write(str1 + str2 + str3 + str4 + str5 + str6 + str7 + str8 + str9 + str10 + str11)
+    f.write(str1 + str2 + str3 + str4)
+
+if __name__ == '__main__':
+    print("hello world")
--- gui/graph_gen.py
+++ gui/graph_gen.py
@@ -6,96 +6,62 @@ from networkx.readwrite import json_graph
 
 import jsm.jsm_analysis as jm  # include our tests
 
-from flask import Flask
-
-def write_html(s):
-
-    reasons_square = []
-    propetis = []
+def writeHtml():
     br = '<br>'
-    save_reas = ''' save_REAS    text'''
-
     f = open('test.html', 'w', encoding='utf-8')
     str1 = '''
-
     <html>
     <head>
     <meta charset="Utf-8">
       <style type="text/css">
       #container {
-        max-width: 800px;
-        height: 800px;
-        margin-left: 15%;
+        min-width: 600px;
+        min-height: 600px;
+        margin-left: 5%;
         background-color:#DFEAFB;
-
       }
       #bigtable {
         background-color: gray;
-
       }
       #lefttext {
         background-color:#FFC3C3;
-
       }
 
     </style>
     </head>
     <body>
-      <table id="bigtable" border="2">
+      <table id="bigtable" border="2" align="center">
       <tr>
-      <td style="min-width:200px; width:15%; background-color:#F7EC8D";>'''
-
-    str2 = '''<div>
-      <p id="result"><b>*** Empty place ***<br></b>'''
-    # for i in data['P']:
-    #     if data[i] != 0 and data[i] !=1:
-    #         str2 += ';    EMPTY PLACE'
-    # str3 = '''<br><b>Необходимо обладание причин: <br></b>''' + save_reas + '''</p></div>'''
-
-    str4 = '''
+        <td style="min-width:200px; width:15%; background-color:#F7EC8D";><div></div>
+      <p id="result"><b>*** Empty place ***<br></b>
       </td>
-      <td style="min-width:810px; width:60%; background-color:#A7FBA9;">
+      <td style="width:80%; background-color:#A7FBA9;">
           <div id="container"></div>
       </td>
 
       <td style="min-width:200px; width:25%";>
       <div style="background-color:#8B98F9">
         <b>*** Empty place ***</b><br>
-      '''
 
-    # for properties
-
-    str5 = ''''''
-    for i in range(len(propetis)):
-        propetis[i] += br
-        str5 += str(i + 1) + propetis[i]
-
-    str6 = '''
       </div>
         <div id="lefttext">
         <b>*** Empty place ***</b><br>
-        '''
-
-    # THIS  PLACE
-    tempstr = ''''''
-    for i in range(len(reasons_square)):
-        reasons_square[i] += br
-        tempstr += str(i) + reasons_square[i]
-
-    str7 = tempstr
-    str8 = ''' </div>
+         </div>
       </td>
+        </tr>
+        <tr style="height: 50%">
+          <td colspan="3" align="center" style="background-color: #FFECEC">LOOOG!!</td>
         </tr>
       </table>
       <script src="sigma.js"></script>
       <script src="sigma.parsers.json.min.js"></script>
       <div id = "myid2" name="text" style="height:1px; width: 100%; visibility:hidden;">
       '''
-    str9 = s
-    str10 = '''
+    str2 = s
+    str3 = '''
       </div>'''
 
-    str11 = '''
+    str4 = '''
       <script>
        var x = document.getElementById("myid2").innerHTML;
       // document.getElementById("result").innerHTML = x;
@@ -118,8 +84,9 @@ def write_html(s):
       <p align="center">end &copy;</p>
     </body>
     </html>
+
     '''
-    f.write(str1 + str2 + str4 + str5 + str6 + str7 + str8 + str9 + str10 + str11)
+    f.write(str1 + str2 + str3 + str4)
 
 def old_draw():
     leng = len(t1)  # count of pairs in hypotheses
@@ -304,7 +271,35 @@ json.dump(d, open('data.json', 'w'))
 s = json.dumps(d)
 # print(s)
 
-write_html(s=s)
+
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("test_template.html",
+                           left1='taken from main.py',
+                           right1='right1',
+                           right2='right2',
+                           graph=s,
+                           variable1="change!!!!")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+writeHtml()
+
+
+
+
+
+
+
 
 # pos = nx.circular_layout(G)
 # nx.draw_networkx(G, pos=pos, with_labels=True)
