d1f758d48582ab93d450da54dd655ae4e28bdedf
fiveh <hunter_hunternabab@mail.ru>
2017-01-22 20:23:07 +0300

    fix bug with test

--- gui/graph_gen.py
+++ gui/graph_gen.py
@@ -11,16 +11,17 @@ _BR_TMPL = 'br'
 
 
 def generate_graph(hypothesis, path, name_reas):
-    # TODO: make job for hypothesis recieved from aqjsm
-    # TODO: fix files square.cfg and ex1.csv
+    # TODO: if run 'graph_gen'  change 'tmpl'
 
     # name_reas = hypothesis
 
     G = nx.Graph()
     colors = ['green', 'darkred', 'brown', 'yellow', 'blue', 'orange']
-    scale = 3  # space near node
-
-    _draw_nodes(G, hypothesis, scale, 8, name_reas)
+    scale = 2  # space near node
+    if (isinstance(hypothesis[0], list) == True):
+        _draw_nodes2(G, hypothesis, scale, 8, name_reas)
+    else:
+        _draw_nodes1(G, hypothesis, scale, 8, name_reas)
 
     d = json_graph.node_link_data(G)
     d['edges'] = d['links']
@@ -33,6 +34,7 @@ def generate_graph(hypothesis, path, name_reas):
 
 def _generate_cause_html(path, s):
     tmpl = open('gui/templates/cause_template.html')
+    # tmpl = open('templates/cause_template.html') # square
     text = tmpl.read()
     # print(text)
     tmpl.close()
@@ -44,7 +46,7 @@ def _generate_cause_html(path, s):
     dest.close()
 
 
-def _draw_nodes(G, hypothesis, scale, size_node, name_reas):
+def _draw_nodes2(G, hypothesis, scale, size_node, name_reas):
     mas_edge = []
     mas_reas = []
     last_id = 0
@@ -68,7 +70,7 @@ def _draw_nodes(G, hypothesis, scale, size_node, name_reas):
                         mas_edge.append(mas_edge[j])
                     else:
                         mas_edge.append(last_id)
-                    lab = name_reas[j+1]
+                    lab = name_reas[j + 1]
                     # lab = 'test'
                     if mas_reas.count(j) < 1:
                         if (count_node % count_line_reas == 0) & (j > 0):
@@ -82,6 +84,57 @@ def _draw_nodes(G, hypothesis, scale, size_node, name_reas):
                 else:
                     mas_edge.append(None)
             mas_reas.append(None)
+        sel_nod_col += 1
+        if sel_nod_col > 1:
+            sel_nod_col = 0
+    num = mas_edge.count(None)
+    for i in range(num):
+        mas_edge.remove(None)
+
+    for i in range(len(mas_reas)):
+        if mas_reas[i] is None:
+            mas_edge.insert(i, None)
+
+    _draw_edges_1(G, colors, mas_edge)
+    # _draw_edges(G, colors) # next variant of draw grah
+
+def _draw_nodes1(G, hypothesis, scale, size_node, name_reas):
+    mas_edge = []
+    mas_reas = []
+    last_id = 0
+    count_node = 0
+    x_t, y_t = 1 * scale, 1 * scale
+
+    colors = ['green', 'darkred', 'brown', 'yellow', 'blue', 'orange']
+    node_colors = ['darkblue', 'purple', 'gray']
+    sel_nod_col = 0  # select color for NODES
+    count_line_reas = 2  # point in line reas
+    smesh_row = 5  # left margin of next row
+
+    d1 = len(hypothesis)  # count of pairs in hypotheses
+    for i in range(d1):
+        lengmass = len(hypothesis[i].value)  # count of reasons
+        for j in range(lengmass):
+            if hypothesis[i].value[j] == True:
+                if mas_reas.count(j) > 0:
+                    mas_edge.append(mas_edge[j])
+                else:
+                    mas_edge.append(last_id)
+                lab = name_reas[j + 1]
+                # lab = 'test'
+                if mas_reas.count(j) < 1:
+                    if (count_node % count_line_reas == 0) & (j > 0):
+                        y_t += 2 * scale
+                        x_t = smesh_row + 1 * scale
+                    G.add_node(last_id, x=x_t, y=y_t, size=size_node, label=lab,
+                               color=node_colors[sel_nod_col])  # add node reas
+                    last_id += 1
+                    x_t += 1 * scale
+                    count_node += 1
+                mas_reas.append(j)
+            else:
+                mas_edge.append(None)
+        mas_reas.append(None)
         sel_nod_col += 1
         if sel_nod_col > 1:
             sel_nod_col = 0
@@ -139,7 +192,6 @@ if __name__ == '__main__':
     name_reas = list(data.columns.values)
     print(name_reas)
     hypothesis = test2()
-    print(hypothesis)
 
     path = 'templates/res.html'
 
